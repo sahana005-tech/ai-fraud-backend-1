ce5dd8fbf97c84c646da6f1d1d89f4f1a7dd6138
sahana005-tech <sahanahb5577@gmail.com>
2025-10-26 18:12:42 +0530

    Create auth.py

---
+++ backend/app/auth.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from .db import SessionLocal
+
+# Config
+SECRET_KEY = "supersecretkey123"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
